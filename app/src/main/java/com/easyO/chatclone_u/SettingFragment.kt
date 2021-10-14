09e3c746d9f8cbe4a43401f864f0ac1f23233ecf
Ljw <ljws93@naver.com>
2021-10-14 17:03:56 +0900

    setUp settingFragment layout(not Fragment.kt)

--- app/src/main/java/com/easyO/chatclone_u/SettingFragment.kt
+++ app/src/main/java/com/easyO/chatclone_u/SettingFragment.kt
@@ -1,18 +1,34 @@
 package com.easyO.chatclone_u
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import android.widget.Toast
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import com.easyO.chatclone_u.databinding.FragmentSettingBinding
-import com.easyO.chatclone_u.viewModel.MainViewModel
 
 class SettingFragment : PreferenceFragmentCompat() {
-    private lateinit var binder : FragmentSettingBinding
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.setting_pref)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.setBackgroundResource(R.drawable.fragment_background)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        if (preference.key == "HashList") {
+            // user clicked the item
+            Toast.makeText(AppClass.context, "Hash Clicked", Toast.LENGTH_SHORT).show()
+
+            return true
+        }else if (preference.key == "About"){
+            Toast.makeText(AppClass.context, "About Clicked", Toast.LENGTH_SHORT).show()
+            return true
+        }else if (preference.key == "Logout"){
+            Toast.makeText(AppClass.context, "Logout Clicked", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        return false
     }
 }
--- app/src/main/java/com/easyO/chatclone_u/pref/ProfileLayout.kt
+++ app/src/main/java/com/easyO/chatclone_u/pref/ProfileLayout.kt
@@ -2,6 +2,8 @@ package com.easyO.chatclone_u.pref
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.preference.Preference
@@ -17,14 +19,20 @@ class ProfileLayout @JvmOverloads constructor(
 ) : Preference(context, attrs, defStyleAttr) {
 
     init {
-        // 어느 layoutResource를 대상으로 할지 설정
+        // 어느 layout Resource를 대상으로 할지 설정
         widgetLayoutResource = R.layout.profile_layout
     }
 
     // 해당 layout에 들어있는 view를 초기화 하고 기능 설정 가능
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
-        Toast.makeText(AppClass.context, "test", Toast.LENGTH_SHORT).show()
-        //holder.itemView.findViewById<TextView>(R.id.name_textView)
+        // container layout에 있는 개별 view에 대해선 listener를 설정하는게 불가능하다
+        // 대신 해당 container layout 전체에 대한 클릭 listener는 지정 가능
+        holder.itemView.setOnClickListener {
+            Toast.makeText(AppClass.context, "dd", Toast.LENGTH_SHORT).show()
+        }
+
+        // 각각의 뷰에 대한 정의는 가능
+        holder.itemView.findViewById<TextView>(R.id.name_textView).text = "dd"
     }
 }
